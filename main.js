b637ee3b818a419bb0c59587878556292f9b8d24
seifer8ff <brady.bcmp@gmail.com>
2017-02-17 14:49:14 -0600

    bathroom page + images
    Added a new page and hooked up links to it. Replace placeholder images with slightly less placeholder images (local instead of pulling from slow external site).

--- main.js
+++ main.js
@@ -18,3 +18,11 @@ function fadeInHidden()	{
 		hiddenElements[i].classList.toggle("fadeInShort");
 	}
 }
+
+// function addLinkHovers()	{
+// 	var links = document.querySelectorAll(".pic-container a");
+// 	console.log(links);
+// 	for (var i = 0; i < links.length; i++)	{
+// 		links[i].addEventListener("hover", )
+// 	}
+// }
