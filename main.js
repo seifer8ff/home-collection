5939281334c39a5ea7eb3456646685609402422d
seifer8ff <brady.bcmp@gmail.com>
2017-02-19 20:58:18 -0600

    updated images + style
    massive styling overhaul, including navbar css, new fade in animations + JS. 
    Images have been updated.

--- main.js
+++ main.js
@@ -5,19 +5,54 @@ init();
 
 
 function init()	{
-	// begin executing after page load
-	window.setTimeout(function(){
-		fadeInHidden();
-	}, 2000);
+	// fade in elements
+	unhideSequence();
+	unhideAll();
 }
 
-function fadeInHidden()	{
-	var hiddenElements = document.querySelectorAll(".hiddenOnLoad");
+
+
+
+// unhides elements one by one
+function unhideSequence()	{
+	// controls how quickly each element is unhidden
+	var timer = 300;
+	// retrieve all hidden elements
+	var hiddenElements = document.querySelectorAll(".fadeInSequence");
+
+	// recursive, anonymous function with closure
+	var unhideElement = (function()	{
+		// counter for hiddenElements array
+		var i = 0;
+
+		// closure function that unhides elements and then calls itself using timeout function
+		return function()	{
+			hiddenElements[i].classList.toggle("fadeInSequence");
+			hiddenElements[i].classList.toggle("fadeInAnim-short");
+
+			i+=1;
+
+			if (hiddenElements[i])	{
+				setTimeout(unhideElement, timer);
+			}
+		}
+		
+	}) ();
+
+	setTimeout(unhideElement, timer);
+}
+
+function unhideAll()	{
+	var hiddenElements = document.querySelectorAll(".fadeIn");
 	for (var i = 0; i < hiddenElements.length; i++)	{
-		hiddenElements[i].classList.toggle("hiddenOnLoad");
-		hiddenElements[i].classList.toggle("fadeInShort");
+		hiddenElements[i].classList.toggle("fadeIn");
+		hiddenElements[i].classList.toggle("fadeInAnim-long");
 	}
 }
+
+// function unhideEach()	{
+	
+// }
 
 // function addLinkHovers()	{
 // 	var links = document.querySelectorAll(".pic-container a");
